469275198b6441c3327ff5de53a08ed0853b19eb
unknown <afumagic3@gmail.com>
2018-07-27 15:48:59 +0700

    add navigation app

---
+++ Screen/HomeScreen.js
@@ -0,0 +1,140 @@
+import React, { Component } from 'react';
+import * as firebase from 'firebase';
+import { View, Text, StyleSheet, Alert } from 'react-native';
+
+import { StackNavigator } from 'react-navigation'
+
+import  BtnFull  from '../components/BtnFull'
+import  BtnTransparent  from '../components/BtnTransparent'
+import  Input  from '../components/Input'
+import  Title  from '../components/Title'
+
+var config = {
+  apiKey: "AIzaSyBevRQtoQ6xc8qJ0EBImxZeCNB684IxBno",
+  authDomain: "cyhfirebase.firebaseapp.com",
+  databaseURL: "https://cyhfirebase.firebaseio.com",
+  projectId: "cyhfirebase",
+  storageBucket: "cyhfirebase.appspot.com",
+  messagingSenderId: "333181013082"
+};
+
+export default class HomeScreen extends Component {
+  static navigationOptions = {
+    title: 'Home'
+  }
+  constructor(props) {
+    super(props);
+
+    firebase.initializeApp(config);
+
+    this.state = {
+      email: '',
+      password: ''
+    }
+  }
+
+  SetEmailVal(val) {
+    this.setState({
+      email: val
+    })
+  }
+
+  SetPasswordVal(val) {
+    this.setState({
+      password: val
+    })
+  }
+
+  SignIn() {
+    firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
+    .then(result => {
+      //console.log('Sign In Successful')
+      Alert.alert('Sign In Successful')
+      var database = firebase.database();
+        database.ref('users/'+result.user.uid).once('value').then(function(snapshot){
+            console.log(snapshot.val())
+        })
+    })
+    .catch(e => {
+      //console.log(e.message)
+      Alert.alert(e.message)
+    })
+  }
+
+  SignUp() {
+    firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password)
+    .then(result => {
+      let usersRef = firebase.database().ref().child("users")
+      usersRef.child(result.user.uid).set({
+        id: result.user.uid,
+        email: this.state.email,
+        btAddr: ''
+      })
+      //console.log(result.user.uid)
+      Alert.alert('Sign Up Successful')
+    })
+    .catch(e => {
+      //console.log(e.message)
+      Alert.alert(e.message)
+    })
+  }
+
+  render() {
+    const { navigate } = this.props.navigation
+    return (
+      <View style={styles.container}>
+
+        <Title text="CYH" />
+        
+        <View style={styles.inputPlace}>
+          
+          <Input 
+            name='email-outline'
+            type='material-community'
+            size={42}
+            textChange={this.SetEmailVal.bind(this)}
+            holder='Email'
+            isPassword={false}
+            />
+
+          <Input 
+            name='lock-outline'
+            type='material-community'
+            size={42}
+            textChange={this.SetPasswordVal.bind(this)}
+            holder='Password'
+            isPassword={true}
+            />
+
+          <BtnFull
+            btnText="Sign In" 
+            signInMethod={this.SignIn.bind(this)}
+            />
+
+          <BtnTransparent 
+            btnText="Sign Up"
+            signUpMethod={this.SignUp.bind(this)}
+            />
+          
+        </View>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, 
+    flexDirection: 'column',
+    backgroundColor: 'rgba(210, 37, 37, 0.74)',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  inputPlace: {
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column'
+  },
+  
+});
